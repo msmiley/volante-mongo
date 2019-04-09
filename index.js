424d9e7ff8575b3a8b3608595129041ed77b6954
Mike Smiley <msmiley@alumni.rice.edu>
2019-04-09 16:56:43 -0400

    work on crud bridge

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const MongoClient = require('mongodb').MongoClient;
+const mongo = require('mongodb')
+const MongoClient = mongo.MongoClient;
 const MongoOplog = require('mongo-oplog');
 
 //
@@ -8,23 +9,41 @@ const MongoOplog = require('mongo-oplog');
 module.exports = {
 	name: 'VolanteMongo',
 	events: {
-		// force connect (only necessary if defaults are used, otherwise, emit a 
+		// force connect (only necessary if defaults are used, otherwise, emit a
 		// 'VolanteMongo.props' event with the proper info)
     'VolanteMongo.connect'() {
       this.connect();
     },
-    'VolanteMongo.watch'(coll) {
-      this.watch(coll);
+    'VolanteMongo.watch'(ns) {
+      this.watch(ns);
+    },
+    // CRUD API overlay
+    'Volante.create'(ns, doc) {
+    	this.handleCrud && this.insertOne(ns, doc);
+    },
+    'Volante.read'(ns, query, callback) {
+    	this.handleCrud && this.find(ns, query, callback);
+    },
+    'Volante.update'(ns, _id, doc) {
+    	this.handleCrud && this.updateOne(ns, _id, doc);
+    },
+    'Volante.delete'(ns, _id) {
+    	this.handleCrud && this.deleteOne(ns, _id);
     },
   },
 	props: {
-		db: null, 	 // the reference to the driver mongo.Db object
-		watched: [], // watched namespaces
+		client: null, // MongoClient object
+		handleCrud: false, // flag whether module should listen for crud events
+		watched: [],  // watched namespaces
     dbhost: '127.0.0.1',
-    dbname: 'test',
-    dbopts: {},
+    dbport: 27017,
+    dbopts: {
+    	useNewUrlParser: true,
+    },
     oplog: false,
     rsname: '$main',
+    retryInterval: 10000,
+    findOptions: {},
   },
 	updated() {
 		this.connect();
@@ -34,19 +53,20 @@ module.exports = {
 		// Process the provided options and connect to mongodb
 		//
 		connect() {
-		  this.log(`Connecting to mongodb at: ${this.dbhost} using db: ${this.dbname}`);
+		  this.$log(`Connecting to mongodb at: ${this.dbhost}`);
 
 		  var fullhost = this.dbhost;
 
 		  // add full mongodb:// schema if not provided
 		  if (!fullhost.match(/^mongodb:\/\/.*/)) {
-		    fullhost = `mongodb://${this.dbhost}/${this.dbname}`;
+		    fullhost = `mongodb://${this.dbhost}:${this.dbport}`;
 		  }
+		  this.$debug(`full mongo url: ${fullhost}`);
 
 		  // initiate connect
 		  MongoClient
-		  .connect(fullhost, this.dbopt)
-		  .then(db => this.success(db))
+		  .connect(fullhost, this.dbopts)
+		  .then(client => this.success(client))
 		  .catch(err => this.error(err));
 		},
 		//
@@ -55,45 +75,113 @@ module.exports = {
 		watch(collection) {
 		  this.oplog = true; // set to true as convenience
 		  if (this.watched.indexOf(collection) === -1) {
-		    this.debug(`watching the ${collection} collection`);
+		    this.$debug(`watching the ${collection} collection`);
 		    this.watched.push(collection);
 		  }
 		},
 		//
 		// Receives the freshly connected db object from the mongodb native driver
 		//
-		success(db) {
-		  this.log(`Connected to mongodb at ${this.dbhost}`);
+		success(client) {
+		  this.$log(`Connected to mongodb at ${this.dbhost}`);
 
 		  // save to instance variable
-		  this.db = db;
+		  this.client = client;
 
-		  this.$emit('VolanteMongo.connected', this.db);
+		  this.$emit('VolanteMongo.connected', this.client);
 		  if (this.oplog && this.watched.length > 0) {
 		    this.tailOplog();
 		  }
 
-		  // error on connection close
-		  this.db.on('close', () => {
-		    this.log(`mongodb disconnected from ${this.dbhost}`);
-		    this.$emit('VolanteMongo.disconnected');
-		  });
+		  // // error on connection close
+		  // this.db.on('close', () => {
+		  //   this.$log(`mongodb disconnected from ${this.dbhost}`);
+		  //   this.$emit('VolanteMongo.disconnected');
+		  // });
+		},
+		error(err) {
+			this.$error(err);
+			this.$log(`retrying in ${this.retryInterval}ms`);
+			setTimeout(() => this.connect(), this.retryInterval);
+		},
+		//
+		// Use mongodb node.js driver find()
+		//
+		find(ns, query, callback) {
+			if (this.client) {
+				let coll = this.getCollection(ns);
+				if (typeof(query) === 'string') {
+					coll.findOne({ _id: mongo.ObjectID(query) }, (err, doc) => {
+						if (err) {
+							this.$error(err);
+						} else {
+							callback && callback(doc);
+						}
+					});
+				} else {
+					coll.find(query, this.findOptions).toArray((err, docs) => {
+						if (err) {
+							this.$error(err);
+						} else {
+							callback && callback(docs);
+						}
+					});
+				}
+			} else {
+				this.$error('db client not ready');
+			}
+		},
+		//
+		// Use mongodb node.js driver insertOne()
+		//
+		insertOne(ns, doc) {
+			if (this.client) {
+				this.getCollection(ns).insertOne(doc, (err, result) => {
+					if (err) {
+						this.$error(err);
+					}
+				});
+			} else {
+				this.$error('db client not ready');
+			}
+		},
+		updateOne(ns, _id, doc) {
+			if (this.client) {
+				this.getCollection(ns).updateOne({ _id: mongo.ObjectID(_id) }, { $set: doc }, (err, result) => {
+					if (err) {
+						this.$error(err);
+					}
+				});
+			} else {
+				this.$error('db client not ready');
+			}
+		},
+		deleteOne(ns, _id) {
+			if (this.client) {
+				this.getCollection(ns).deleteOne({ _id: mongo.ObjectID(_id) }, (err, result) => {
+					if (err) {
+						this.$error(err);
+					}
+				});
+			} else {
+				this.$error('db client not ready');
+			}
 		},
 		//
 		// Start tailing the mongodb oplog
 		//
 		tailOplog() {
-		  this.debug("initializing oplog connection");
+		  this.$debug("initializing oplog connection");
 
 		  this.mongoOplog = MongoOplog(`mongodb://${this.dbhost}/local`, {
-		    ns: `${this.db.s.databaseName}.(${this.watched.join('|')})`,
+		    ns: `(${this.watched.join('|')})`,
 		    coll: `oplog.${this.rsname}`
 		  });
 
 		  this.mongoOplog.on('insert', (doc) => {
 		    this.$emit(`VolanteMongo.insert`, {
 		      ns: doc.ns,
-		      coll: this.getCollection(doc.ns),
+		      coll: this.splitNamespace(doc.ns)[1],
 		      _id: doc.o._id,
 		      o: doc.o
 		    });
@@ -102,7 +190,7 @@ module.exports = {
 		  this.mongoOplog.on('update', (doc) => {
 		    this.$emit(`VolanteMongo.update`, {
 		      ns: doc.ns,
-		      coll: this.getCollection(doc.ns),
+		      coll: this.splitNamespace(doc.ns)[1],
 		      _id: doc.o2._id, // use the o2 object instead
 		      o: doc.o
 		    });
@@ -111,26 +199,33 @@ module.exports = {
 		  this.mongoOplog.on('delete', (doc) => {
 		    this.$emit(`VolanteMongo.delete`, {
 		      ns: doc.ns,
-		      coll: this.getCollection(doc.ns),
+		      coll: this.splitNamespace(doc.ns)[1],
 		      _id: doc.o._id,
 		      o: doc.o
 		    });
 		  });
 
 		  this.mongoOplog.on('error', (err) => {
-		    this.error(err);
+		  	// ignore certain errors
+		  	if (err.message === "No more documents in tailed cursor") return;
+		    this.$error(err);
 		  });
 
 		  // start the oplog tailing
 		  this.mongoOplog.tail()
-		  .then(() => this.debug('oplog tailing started'))
-		  .catch((err) => this.error(err));
+		  .then(() => this.$debug('oplog tailing started'))
+		  .catch((err) => this.$error(err));
 		},
 		//
-		// Get collection name from full namespace
+		// split namespace into db and collection name
 		//
+		splitNamespace(ns) {
+			let s = ns.split('.');
+		  return [s[0], s.splice(1).join('.')];
+		},
 		getCollection(ns) {
-		  return ns.split('.').pop();
-		},
+			let sns = this.splitNamespace(ns);
+			return this.client.db(sns[0]).collection(sns[1]);
+		}
 	},
-}
+};
